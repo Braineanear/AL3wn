ab396d21b600d475ca2d1c4038d7a6173ddef8da
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-08-30 08:44:53 +0200

    translated the home page

--- al3wn/settings.py
+++ al3wn/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar' #'en-us'
 
 TIME_ZONE = 'Africa/Cairo'
 
--- core/views.py
+++ core/views.py
@@ -1,33 +1,34 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 
 def home(request):
-	context = {'title' : 'Home'}
+	context = {'title' : _('Home')}
 	return render (request, 'core/index.html', context)
 
 def about(request):
-	context = {'title' : 'About Us'}
+	context = {'title' : _('About Us')}
 	return render (request, 'core/about.html', context)
 
 def dev(request):
 	return render (request, 'core/dev.html')
 
 def price(request):
-	context = {'title' : 'Pricing'}
+	context = {'title' : _('Pricing')}
 	return render (request, 'core/price.html', context)
 
 def dashboard(request):
-	context = {'title' : 'Dashboard'}
+	context = {'title' : _('Dashboard')}
 	return render (request, 'core/dashboard.html', context)
 
 def sec(request):
-	context = {'title' : 'Secondary'}
+	context = {'title' : _('Secondary')}
 	return render (request, 'core/secondary.html', context)
 
 def stage1(request):
-	context = {'title' : 'Stage 1'}
+	context = {'title' : _('Stage 1')}
 	return render (request, 'core/stage1.html', context)
 
 def stage2(request):
-	context = {'title' : 'Stage 2'}
+	context = {'title' : _('Stage 2')}
 	return render (request, 'core/stage2.html', context)
