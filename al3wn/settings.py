d7f5bfa19b0771feb467b5450798f5a3d0d74369
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-10-03 17:25:26 +0200

    test6

--- al3wn/settings.py
+++ al3wn/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('AL3WN_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('BEBUG_VALUE')
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
