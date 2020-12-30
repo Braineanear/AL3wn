3d021d4afa755b3dc4ec0f35ce3607230d94dafc
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-12-30 19:49:08 +0200

    made the username a title for profile page

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext as _
 
@@ -36,7 +36,8 @@ def profile(request):
 
 	context = {
 		'user_form': user_form,
-		'profile_form': profile_form
+		'profile_form': profile_form,
+		'title' : request.user.username
 	}
 	return render(request, 'users/profile.html', context)
 
