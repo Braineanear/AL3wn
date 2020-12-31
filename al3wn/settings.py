02e2efa4a2db5fbbacbf85a1bede71ef3403de4d
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-12-31 08:25:13 +0200

    changed colors in admin

--- al3wn/settings.py
+++ al3wn/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fb_bot.apps.FbBotConfig',
-    'core.apps.CoreConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
     'storages',
