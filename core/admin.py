9f02a0684b9fc55d6f7401dc08a711f138f9a278
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-01-09 18:53:41 +0200

    added MrEhabURL to admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import HalemURL, HerrShadyURL, HerrAliURL
+from .models import HalemURL, HerrShadyURL, HerrAliURL, MrEhabURL
 
 admin.site.register(HalemURL)
+admin.site.register(MrEhabURL)
 admin.site.register(HerrAliURL)
 admin.site.register(HerrShadyURL)
