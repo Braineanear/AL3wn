0c9d41bd65962f98ba92524f8d45f2cb6fe94022
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-01-27 04:07:28 +0200

    added OuterExam model and starter admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import HalemURL, HerrShadyURL, HerrAliURL, MrEhabURL, HerrMURL, BassemURL, BassemYouTubeURL
+from .models import OuterExam, HalemURL, HerrShadyURL, HerrAliURL, MrEhabURL, HerrMURL, BassemURL, BassemYouTubeURL
 
 admin.site.register(HalemURL)
 admin.site.register(HerrMURL)
@@ -9,3 +9,10 @@ admin.site.register(BassemYouTubeURL)
 admin.site.register(MrEhabURL)
 admin.site.register(HerrAliURL)
 admin.site.register(HerrShadyURL)
+
+class OuterExamAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publisher', 'year', 'minutes_of_exam', 'exam_day', 'done']
+    ordering = ('date_posted',)
+    list_filter = ('publisher', 'writer', 'done',)
+
+admin.site.register(OuterExam, OuterExamAdmin)
--- core/models.py
+++ core/models.py
@@ -10,6 +10,42 @@ Bassem_TYPES = (
 	('perfect', 'perfect')
 	)
 
+PUBLISHERS = (
+	('EE', 'Mr. Ehab El Shafey'),
+	('GA', 'Herr Ali Rashed'),
+	('GM', 'Herr Mohammed Abdel Atty'),
+	('AH', 'Mohammed Abdel Halem'),
+	('GS', 'Herr Shady')
+	)
+
+WRITERS = (
+	('AMS', 'AMS'),
+	('AA', 'Ashraf'),
+	('HA', 'Hala'),
+	('MM', 'Maryam'),
+	('MG', 'Gouda')
+	)
+
+SCHOOL_YEAR = (
+	('1', '1'),
+	('2', '2'),
+	('3', '3')
+	)
+
+class OuterExam(models.Model):
+	title = models.CharField(max_length=255)
+	writer = models.CharField(_('Writer'), choices=WRITERS, max_length=255)
+	publisher = models.CharField(_('Publisher'), choices=PUBLISHERS, max_length=255)
+	year = models.CharField(_('Academic Year'), choices=SCHOOL_YEAR, max_length=255)
+	link = models.URLField(max_length=512)
+	exam_day = models.DateField(blank=True, null=True)
+	minutes_of_exam = models.IntegerField(blank=True, null=True)
+	date_posted = models.DateTimeField(default=timezone.now)
+	done = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.title
+
 class HalemURL(models.Model):
 	link = models.URLField(default='http://al3wn.com/', max_length=512)
 
