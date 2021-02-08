909944296f075c99f4700395d5f1c7c3469a2449
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-02-08 03:18:53 +0200

    added a nprofile/

--- users/views.py
+++ users/views.py
@@ -20,7 +20,7 @@ def register(request):
 
 
 @login_required
-def profile(request):
+def oprofile(request):
 	if request.method == "POST":
 		user_form = UserUpdateForm(request.POST, instance=request.user)
 		profile_form = ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
@@ -41,6 +41,27 @@ def profile(request):
 	}
 	return render(request, 'users/oprofile.html', context)
 
+@login_required
+def profile(request):
+	if request.method == "POST":
+		user_form = UserUpdateForm(request.POST, instance=request.user)
+		profile_form = ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, f'Your account has been Updated')
+			return redirect('Profile')
+	else:
+		user_form = UserUpdateForm(instance=request.user)
+		profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+	context = {
+		'user_form': user_form,
+		'profile_form': profile_form,
+		'title' : request.user.username
+	}
+	return render(request, 'users/profile.html', context)
 
 def career(request):
 	if request.method == "POST":
