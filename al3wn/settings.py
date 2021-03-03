e75a585cd7b8af4a18feceeb0cd432f5157d604e
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-03-04 00:54:08 +0200

    made the shortner app

--- al3wn/settings.py
+++ al3wn/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fb_bot.apps.FbBotConfig',
     'users.apps.UsersConfig',
+    'shortner.apps.ShortnerConfig',
     'crispy_forms',
     'storages',
 
