a2bf152beab108d24d6f0863bc59754b2c6b2533
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-04-25 20:43:23 +0200

    Adding Dark Mode Script Part

--- core/static/core/js/script.js
+++ core/static/core/js/script.js
@@ -2,6 +2,37 @@
 
 	'use strict';
 
+	
+	const darkBtn = document.querySelector('.fa-moon-o');
+	const bodyEl = document.querySelector('body');
+
+	const darkMode = () => {
+		bodyEl.classList.toggle( 'dark' )
+	}
+
+	darkBtn.addEventListener( 'click', () => {
+		// Get the value of the "dark" item from the local storage on every click
+		setDarkMode = localStorage.getItem( 'dark' );
+
+		if ( setDarkMode !== "on" ) {
+			darkMode();
+			// Set the value of the itwm to "on" when dark mode is on
+			setDarkMode = localStorage.setItem( 'dark', 'on' );
+		} else {
+			darkMode();
+			// Set the value of the item to  "null" when dark mode if off
+			setDarkMode = localStorage.setItem( 'dark', null );
+		}
+	} );
+
+	// Get the value of the "dark" item from the local storage
+	let setDarkMode = localStorage.getItem( 'dark' );
+
+	// Check dark mode is on or off on page reload
+	if ( setDarkMode === 'on' ) {
+		darkMode();
+	}
+	
 	var countDownDate = new Date("April 15, 2020 00:00:00").getTime();
 
 	var x = setInterval(function () {
