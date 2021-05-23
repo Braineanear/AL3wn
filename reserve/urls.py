26c1a0867e42bdd547386113d939208879d92f22
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-05-23 12:03:23 +0200

    added teacher list view url

--- reserve/urls.py
+++ reserve/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns  =  [
 	path("", views.Home.as_view(), name='ReserveHome'),
 	path("view/", views.ApplicantListView.as_view(), name='view'),
+	path("view/<slug:teacher>", views.ApplicantListView.as_view()),
 	path("report/", views.render_pdf_view, name='report'),
 	path("report/<uuid>", views.applicant_render_pdf_view, name='Report'),
 	path("<slug:teacher>/<slug:year>/", views.All),
