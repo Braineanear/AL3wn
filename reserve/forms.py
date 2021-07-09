06dfb87c5016917d4eb511f7f58fd97af4cd7859
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-07-09 14:19:42 +0200

    added subject filter form

--- reserve/forms.py
+++ reserve/forms.py
@@ -16,3 +16,31 @@ class ApplicantForm(forms.ModelForm):
 		model = Applicant
 		fields = ['name', 'gender', 'email','address' ,'phone_number', 'a_phone_number',
 		'school_type', 'classe']
+
+
+
+class FilterForm(forms.Form):
+	SUBJECT_CHOICE = (
+	("Arabic",_("Arabic")),
+	("English",_("English")),
+	("biology",_("biology")),
+	("Geology",_("Geology")),
+	("Maths",_("Maths")),
+	("science",_("science")),
+	("Deutsch",_("Deutsch")),
+	("French",_("French")),
+	('Italian', _('Italian')),
+	('Statistics', _('Statistics')),
+	('Physics', _('Physics')),
+	('Chemistry', _('Chemistry')),
+	('philosophy', _('philosophy')),
+	('psychology', _('psychology')),
+	('Geography', _('Geography')),
+	('history', _('history')),
+	('Chinese', _('Chinese')),
+	('Spanish', _('Spanish')),
+	('Studies', _('Studies')),
+	('None', 'None'),
+	)
+
+	subject = forms.ChoiceField(label=_('Subject'), choices=SUBJECT_CHOICE)
