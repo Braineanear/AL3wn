6deb30e5a01579a29ad4d337995af97f28abbe6d
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-07-09 14:20:43 +0200

    added names for all URLs + added subject filter url

--- reserve/urls.py
+++ reserve/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns  =  [
 	path("", views.Home.as_view(), name='ReserveHome'),
+	path("<str:subject>/", views.SubjectFilter.as_view(), name='ReserveSubject'),
 	path("view/", views.ApplicantListView.as_view(), name='view'),
-	path("view/<slug:teacher>", views.ApplicantListView.as_view()),
+	path("view/<slug:teacher>", views.ApplicantListView.as_view(), name='ViewTeacher'),
 	path("view/<slug:teacher>/excel/", views.excelview, name='Excel'),
 	path("report/", views.render_pdf_view, name='report'),
 	path("report/<uuid>", views.applicant_render_pdf_view, name='Report'),
-	path("<slug:teacher>/<slug:year>/", views.All),
+	path("<slug:teacher>/<slug:year>/", views.All ,name='Form'),
 ]
