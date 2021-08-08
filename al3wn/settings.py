bdbc1aca4f0ea2bd7a4b097edaa6d9d69a1ca9e8
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-08-08 13:28:46 +0200

    added a starter report app

--- al3wn/settings.py
+++ al3wn/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'reserve.apps.ReserveConfig',
+    'report.apps.ReportConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
